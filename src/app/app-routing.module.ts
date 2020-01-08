230b85d6531eff7ecafbc4fd3e1e98525827403a
ghanuisnotover <gs8111998@gmail.com>
2020-01-08 12:34:16 +0530

    Angular cart

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { FooterComponent } from './footer/footer.component';
+
+
+const routes: Routes = [
+  {path:'footer',component:FooterComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
